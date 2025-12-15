ebea3cf4d154aecdda166dadfc2053bf73be9ee1
portegys <portegys@gmail.com>
2025-12-14 22:32:43 -0600

    RNN

--- work/build.sh
+++ work/build.sh
@@ -2,5 +2,4 @@
 javac -classpath "../lib/*" -d . `find ../src -name '*.java' -print`
 cp ../src/main/python/causations_nn.py .
 cp ../src/main/python/causations_rnn.py .
-cp ../src/main/python/attention.py .
-jar cvfm ../bin/mandala.jar mandala.mf mandala morphognosis rdtree causations_nn.py causations_rnn.py attention.py
+jar cvfm ../bin/mandala.jar mandala.mf mandala morphognosis rdtree causations_nn.py causations_rnn.py
